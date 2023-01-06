df0db8d9f23b74625c44983ca7b1d7db78aef4e9
Din A <52484378+kkingslayer@users.noreply.github.com>
2023-01-06 20:43:33 +0300

    add remove old dataset

--- fordockertrain.py
+++ fordockertrain.py
@@ -1,11 +1,12 @@
 import os
+import shutil
 
 os.system("git clone https://github.com/ultralytics/yolov5")
 
 os.chdir("yolov5/")
 os.system("pip install -r requirements.txt")
 
-
+shutil.rmtree("vegsdataset")
 os.system("git clone https://github.com/kkingslayer/vegsdataset")
 
 #os.system("python3 train.py --epochs 1 --data inf.yaml --weights yolov5s.pt")
