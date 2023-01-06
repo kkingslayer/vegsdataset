7baf3aede97ae822cd6f7d43a49f6101694e0714
Din A <52484378+kkingslayer@users.noreply.github.com>
2023-01-06 20:32:33 +0300

    add git download

--- fordockertrain.py
+++ fordockertrain.py
@@ -1,2 +1,12 @@
 import os
+
+os.system("git clone https://github.com/ultralytics/yolov5")
+
+os.chdir("yolov5/")
+os.system("pip install -r requirements.txt")
+
+
+os.system("git clone https://github.com/kkingslayer/vegsdataset")
+
 #os.system("python3 train.py --epochs 1 --data inf.yaml --weights yolov5s.pt")
+
